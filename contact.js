3ce0b091e91d40eb3a06fa600fc2efcdca2fa25f
Nibal6 <nibalmalaeb2003@gmail.com>
2024-12-08 10:59:44 +0200

    Amendmentments on Local Storage in Contact.js

--- contact.js
+++ contact.js
@@ -16,3 +16,29 @@ inputs.forEach((input) => {
   input.addEventListener("focus", focusFunc);
   input.addEventListener("blur", blurFunc);
 });
+
+
+let submitButton = document.querySelector(".btn");
+submitButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  let check = true;
+
+  
+  inputs.forEach((input) => {
+    if (input.value.trim() === "") {
+      check = false;
+    }
+  });
+
+  let textarea = document.querySelector("textarea");
+  if (textarea.value.trim() === "") {
+    check = false;
+  }
+
+  
+  if (check) {
+    alert("Thank you for reaching out; we’ll get back to you soon.");
+  } else {
+    alert("Please fill out all forms.");
+  }
+});
